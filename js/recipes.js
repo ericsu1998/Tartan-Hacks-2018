1878e221f0163a531f9d253ff5f82dbaa31efacb
Eric Su <esu1@andrew.cmu.edu>
2018-02-11 23:06:06 -0500

    Added more recipes, think about format though

--- js/recipes.js
+++ js/recipes.js
@@ -1,14 +1,166 @@
 recipe_list = {
+	//Meat category
+	"Teriyaki_Pork_Donburi" : {
+		"ingredients" : [],
+		"picture" : "",
+		"tags" : [
+		]
+	},
+
+	"breaded_chicken" : {
+		"ingredients" : [ingredients],
+		"picture" : "picture name",
+		"tags" : [
+			"tag1",
+			"tag2",
+			"tag3"
+		]
+	},
+
+	"Shougayaki" : {
+		"ingredients" : [ingredients],
+		"picture" : "picture name",
+		"tags" : [
+			"tag1",
+			"tag2",
+			"tag3"
+		]
+	},	
+
+	"Skillet_Chicken_Bulgogi" : {
+		"ingredients" : [ingredients],
+		"picture" : "picture name",
+		"tags" : [
+			"tag1",
+			"tag2",
+			"tag3"
+		]
+	},
+
+	//Noods Category
+	"Niku_Udon" : {
+		"ingredients" : [ingredients],
+		"picture" : "picture name",
+		"tags" : [
+			"tag1",
+			"tag2",
+			"tag3"
+		]
+	},
+
+	"Yakisoba" : {
+		"ingredients" : [ingredients],
+		"picture" : "picture name",
+		"tags" : [
+			"tag1",
+			"tag2",
+			"tag3"
+		]
+	},
+
+	"Cheesy_Broccoli_Pasta_Dish" : {
+		"ingredients" : [ingredients],
+		"picture" : "picture name",
+		"tags" : [
+			"tag1",
+			"tag2",
+			"tag3"
+		]
+	},
+
+	//Vegetable?? Category
 	"mapo_tofu" : {
 		"ingredients" : [],
 		"picture" : "mapoTofu.jpg",
 		"tags" : [
 			"tofu",
 			"beef",
-			"mapo"
 		]
-	}
+	},
+
+	"Garlic_Brussel_Sprouts" : {
+		"ingredients" : [],
+		"picture" : "mapoTofu.jpg",
+		"tags" : [
+			"tofu",
+			"beef",
+		]
+	},	
+
+	"Roasted_Paremesan_Kohlrabi" : {
+		"ingredients" : [],
+		"picture" : "mapoTofu.jpg",
+		"tags" : [
+			"tofu",
+			"beef",
+		]
+	},	
+
+	"Daikon_Cake" : {
+		"ingredients" : [],
+		"picture" : "mapoTofu.jpg",
+		"tags" : [
+			"tofu",
+			"beef",
+		]
+	},	
+
+	"Roasted_Rutebaga" : {
+		"ingredients" : [],
+		"picture" : "mapoTofu.jpg",
+		"tags" : [
+			"tofu",
+			"beef",
+		]
+	},
+
+	"Steamed_Broccoli" : {
+		"ingredients" : [],
+		"picture" : "mapoTofu.jpg",
+		"tags" : [
+			"tofu",
+			"beef",
+		]
+	},
+
+	"Steamed_Cauliflower" : {
+		"ingredients" : [],
+		"picture" : "mapoTofu.jpg",
+		"tags" : [
+			"tofu",
+			"beef",
+		]
+	},
+
+	"Cauliflower/Cucumber/Grape_Salad" : {
+		"ingredients" : [],
+		"picture" : "mapoTofu.jpg",
+		"tags" : [
+			"tofu",
+			"beef",
+		]
+	},
+
+	"Tomato_Egg" : {
+		"ingredients" : [],
+		"picture" : "mapoTofu.jpg",
+		"tags" : [
+			"tofu",
+			"beef",
+		]
+	},
 }
+
+//Input format
+	// "recipe_name" : {
+	// 	"ingredients" : [ingredients],
+	// 	"picture" : "picture name",
+	// 	"tags" : [
+	// 		"tag1",
+	// 		"tag2",
+	// 		"tag3"
+	// 	]
+	// },
 
 //Recipe format
 //1. Dish name
